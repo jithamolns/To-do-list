d87741960a5023f0a637b763775f78a437e00730
jithamolns <jithamolns@gmail.com>
2023-07-04 15:12:51 +0100

    app setup

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'to-do-list';
+  title = 'To Do List Application';
+  err:string = '';
+  list:any[] = [];
+
+  addTask(item:string){
+    if(item!=''){
+      this.list.push({id:this.list.length,name:item});
+      this.err = '';
+    }else{
+      this.err = 'Please add a task';
+    }
+  }
+  removeTask(id:number){
+    this.list=this.list.filter(item=>item.id!==id);
+  }
 }
