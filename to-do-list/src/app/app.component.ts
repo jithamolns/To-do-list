d67fef7f57655b2633fcb883fbabdabb8a03dc59
jithamolns <jithamolns@gmail.com>
2023-07-10 17:24:47 +0100

    Basic code removed

--- to-do-list/src/app/app.component.ts
+++ to-do-list/src/app/app.component.ts
@@ -6,23 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'To Do List';
-  err:string = '';
-  list:any[] = [];
-  completedList:any[] = [];
-
-  addTask(item:string){
-    if(item!=''){
-      this.list.push({id:this.list.length,name:item});
-      this.err = '';
-    }else{
-      this.err = 'Please add a task';
-    }
-  }
-  removeTask(id:number){
-    this.list = this.list.filter(item=>item.id!==id);
-  }
-  completeTask(taskId:number, taskName:string){
-    this.completedList.push({taskId:this.completedList.length,name:taskName});
-  }
+  
 }
